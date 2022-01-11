6a614b4aa0492a35f7702a5c66d0cf3e578f04d8
Charles Archibong <info@charlesarchibong.copm>
2022-01-11 04:40:00 +0100

    learning unit testing with mocha in JS

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,25 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const { it } = require('mocha');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+
+
+class Car {
+    park() {
+        return 'parked';
+    }
+
+    drive() {
+        return 'driving';
+    }
+}
+
+
+describe('Car', () => {
+    it('can park', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'parked');
+    })
+})
